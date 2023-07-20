2cc34dd218f4e2f0d3d89ab1c8f2263b691b98d6
jv699 <jvacarch@gmail.com>
2023-07-19 20:16:21 -0400

    still cracking

--- Arrays.js
+++ Arrays.js
@@ -6,7 +6,16 @@ function isEmpty(arr) {
     return false
 }
 
+//adding elements
 function setSecondElementToZero(nums) {
   nums[1] = 0;
   return undefined;
+}
+
+//accessing elements
+function at(arr, i) {
+  if (i >= 0 && i < arr.length)
+    return arr[i];
+  else
+    return null;
 }
