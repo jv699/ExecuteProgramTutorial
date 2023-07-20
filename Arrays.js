0195a60c39713c97c56bd909df3d7b93e1f6ae1b
jv699 <jvacarch@gmail.com>
2023-07-19 20:15:58 -0400

    still cracking

---
+++ Arrays.js
@@ -0,0 +1,12 @@
+// Empty test
+function isEmpty(arr) {
+  if (arr.length == 0){
+    return true;
+  } else
+    return false
+}
+
+function setSecondElementToZero(nums) {
+  nums[1] = 0;
+  return undefined;
+}
